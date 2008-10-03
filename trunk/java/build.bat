3da161dc91aa94594ec5180a404851a81b3941f4
charlesw123456 <charlesw123456@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2008-10-03 05:30:12 +0000

    * Change namespace.

--- trunk/java/build.bat
+++ trunk/java/build.bat
@@ -1,4 +1,4 @@
-javac -source 1.4 -target 1.4 -d . Trace.java Scanner.java Tab.java DFA.java ParserGen.java Parser.java Coco.java
-jar cfm Coco.jar Coco.manifest Coco/*.class
-del Coco\*.class
-rd Coco
+javac -source 1.4 -target 1.4 -d . Trace.java Scanner.java Tab.java DFA.java ParserGen.java Parser.java CocoXml.java
+jar cfm CocoXml.jar CocoXml.manifest CocoXml/*.class
+del CocoXml\*.class
+rd CocoXml
