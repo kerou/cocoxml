417f15c06560af0eff48fe598e349287f94d6919
charlesw123456 <charlesw123456@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2008-10-04 06:34:33 +0000

    * It seems that I have finish XSData.java. Pass the compilation, not tested yet.

--- java/XSData.java
+++ java/XSData.java
@@ -19,7 +19,16 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 package CocoXml;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Hashtable;
+import java.util.Map;
 
 class TagInfo {
     public int startToken;
@@ -39,41 +48,190 @@ enum Options {
 }
 
 class XmlLangDefinition {
-    Tab    tab;
-    Errors errors;
+    Tab                        tab;
+    Errors                     errors;
+    boolean[]                  useVector;
+    Hashtable<String, TagInfo> Tags;
+    Hashtable<String, Integer> Attrs;
 
     public XmlLangDefinition(Tab tab, Errors errors) {
 	this.tab = tab;
 	this.errors = errors;
+	useVector = new boolean[Options.values().length];
+	Tags = new Hashtable<String, TagInfo>();
+	Attrs = new Hashtable<String, Integer>();
     }
 
     public void AddOption(String optName, int line) {
+	int optval; Symbol sym;
+	try {
+	    optval = Options.valueOf(optName).ordinal();
+	} catch (IllegalArgumentException e) {
+	    errors.SemErr("Unsupported option '" + optName + "' encountered.");
+	    return;
+	}
+	useVector[optval] = true;
+	sym = tab.FindSym(optName);
+	if (sym == null) tab.NewSym(Node.t, optName, line);
+    }
+
+    int addUniqueToken(String tokenName, String typeName, int line) {
+	Symbol sym = tab.FindSym(tokenName);
+	if (sym != null)
+	    errors.SemErr(typeName + " '" + tokenName + "' declared twice");
+	sym = tab.NewSym(Node.t, tokenName, line);
+	return sym.n;
     }
 
     public void AddTag(String tagName, String tokenName, int line) {
+	TagInfo tinfo = new TagInfo();
+	tinfo.startToken = addUniqueToken(tokenName, "Tag", line);
+	tinfo.endToken = addUniqueToken("END_" + tokenName, "Tag", line);
+
+	if (Tags.containsKey(tagName))
+	    errors.SemErr("Tag '" + tagName + "' declared twice");
+	Tags.put(tagName, tinfo);
     }
 
     public void AddAttr(String attrName, String tokenName, int line) {
+	int value = addUniqueToken(tokenName, "Attribute", line);
+	if (Attrs.containsKey(attrName))
+	    errors.SemErr("Attribute '" + attrName + "' declared twice");
+	Attrs.put(attrName, value);
     }
 
     public void Write(PrintWriter gen) {
+	for (int option = 0; option < useVector.length; ++option)
+	    if (useVector[option])
+		gen.printf("\tcurXLDef.useVector[%d] = true;\n", option);
+	for (Map.Entry<String, TagInfo> me : Tags.entrySet())
+	    gen.printf("\tcurXLDef.AddTag(%s, %d, %d);\n", me.getKey(),
+		       me.getValue().startToken, me.getValue().endToken);
+	for (Map.Entry<String, Integer> me : Attrs.entrySet())
+	    gen.printf("\tcurXLDef.AddAttr(%s, %d);\n",
+		       me.getKey(), me.getValue());
     }
 }
 
 class XmlScannerData {
     public static final int EOF = -1;
 
-    Tab     tab;
-    Errors  errors;
+    Tab         tab;
+    Errors      errors;
+    Reader      fram;
+    PrintWriter gen;
+
+    Hashtable<String, XmlLangDefinition>  XmlLangMap;
 
     public XmlScannerData(Parser parser) {
 	tab = parser.tab;
 	errors = parser.errors;
+	XmlLangMap = new Hashtable<String, XmlLangDefinition>();
     }
 
     public void Add(String NamespaceURI, XmlLangDefinition xldef) {
+	if (XmlLangMap.containsKey(NamespaceURI))
+	    errors.SemErr("Namespace '" + NamespaceURI + "' declared twice");
+	XmlLangMap.put(NamespaceURI, xldef);
+    }
+
+    void OpenGen(boolean backUp) {
+	try {
+	    File f = new File(tab.outDir, "Scanner.java");
+	    if (backUp && f.exists()) {
+		File old = new File(f.getPath() + ".old");
+		old.delete(); f.renameTo(old);
+	    }
+	    gen = new PrintWriter(new BufferedWriter(new FileWriter(f, false))); /* pdt */
+	} catch (Exception e) {
+	    throw new FatalError("Cannot generate scanner file.");
+	}
+    }
+
+    int framRead() {
+	try {
+	    return fram.read();
+	} catch (java.io.IOException e) {
+	    throw new FatalError("Error reading Scanner.frame");
+	}
+    }
+
+    void CopyFramePart(String stop) {
+	char startCh = stop.charAt(0);
+	int endOfStopString = stop.length() - 1;
+	int ch = framRead();
+	while (ch != EOF)
+	    if (ch == startCh) {
+		int i = 0;
+		do {
+		    if (i == endOfStopString) return; // stop[0..i] found
+		    ch = framRead(); i++;
+		} while (ch == stop.charAt(i));
+		// stop[0..i-1] found; continue with last read character
+		gen.print(stop.substring(0, i));
+	    } else {
+		gen.print((char)ch); ch = framRead();
+	    }
+	throw new FatalError("Incomplete or corrupt scanner frame file");
+    }
+
+    void WriteOptions() {
+	gen.println("    public enum Options {");
+	for (Options opt: Options.values())
+	    gen.printf("        %s,\n", opt.name());
+	gen.println("    };");
+	gen.printf("    public final int numOptions = %d;\n",
+		   Options.values().length);
+    }
+
+    void WriteDeclarations() {
+	Symbol sym;
+	gen.println("    static readonly int[] useKindVector = new int[] {");
+	for (Options opt: Options.values()) {
+	    sym = tab.FindSym(opt.name());
+	    gen.printf("        %d,\n", sym == null ? -1 : sym.n);
+	}
+	gen.println("    };");
+    }
+
+    void WriteInitialization() {
+	for (Map.Entry<String, XmlLangDefinition> me: XmlLangMap.entrySet()) {
+	    gen.println("\tcurXLDef = new XmlLangDefinition();");
+	    me.getValue().Write(gen);
+	    gen.printf("\tXmlLangMap.Add(\"%s\", curXLDef);\n", me.getKey());
+	}
     }
 
     public void WriteXmlScanner() {
+	int i, j;
+	File fr = new File(tab.srcDir, "Scanner.frame");
+	if (!fr.exists()) {
+	    if (tab.frameDir != null)
+		fr = new File(tab.frameDir.trim(), "Scanner.frame");
+	    if (!fr.exists())
+		throw new FatalError("Cannot find Scanner.frame");
+	}
+	try {
+	    fram = new BufferedReader(new FileReader(fr)); /* pdt */
+	} catch (FileNotFoundException e) {
+	    throw new FatalError("Cannot open Scanner.frame.");
+	}
+	OpenGen(true);
+	CopyFramePart("/*---- Begin ----*/");
+	gen.close();
+	OpenGen(false);
+	gen.println("// THIS FILE IS GENERATED BY CocoXml AUTOMATICALLY, DO NOT EDIT IT MANUALLY.");
+	CopyFramePart("/*---- Namespace ----*/");
+	if (tab.nsName != null && tab.nsName.length() > 0)
+	    gen.printf("package %s;\n", tab.nsName);
+
+	CopyFramePart("/*---- Options ----*/"); WriteOptions();
+
+	CopyFramePart("/*---- Declarations ----*/"); WriteDeclarations();
+
+	CopyFramePart("/*---- Initialization ----*/"); WriteInitialization();
+
+	CopyFramePart("/*---- $$$ ----*/");
+	gen.close();
     }
 }
