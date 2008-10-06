15bce64fc8bd79dfca16063e6e2aad9867f19422
charlesw123456 <charlesw123456@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2008-10-06 14:36:47 +0000

    * Package rss.jar in samples. * Refine and link all build.xml into one.

--- trunk/java/samples/RssMain.java
+++ trunk/java/samples/RssMain.java
@@ -16,6 +16,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 -------------------------------------------------------------------------*/
 
+package rss;
+
 public class RssMain {
     static void ParseRSS(String filename) {
 	try {
