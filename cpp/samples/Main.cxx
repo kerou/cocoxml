9b59b9d93a6eb729f0d1e17705d419c47f7ee6c5
kentoo.zhao <kentoo.zhao@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2008-10-27 15:15:41 +0000

    -> instead of .

--- cpp/samples/Main.cxx
+++ cpp/samples/Main.cxx
@@ -1,13 +1,15 @@
 #include <iostream>
-#include "XmlParser.h"
-#include "XmlScanner.h"
+#include "XmlParser.hxx"
+#include "XmlScanner.hxx"
 #include "RSSData.hxx"
+
+using namespace cocoxml;
 
 int main(int argc, char **argv)
 {
-    XmlScanner scanner = new XmlScanner(filename);                                                                   
-    XmlParser parser = new XmlParser(scanner);
+    XmlScanner *scanner = new XmlScanner(coco_string_create(argv[1]));
+    XmlParser *parser = new XmlParser(scanner);
     
-    parser.Parse();
-    std::cout << parser.rss << std::endl;
+    parser->Parse();
+    std::cout << parser->rss << std::endl;
 }
