66a91fb3283e9538156ef4288a9b7b9ba99d9b9b
kentoo.zhao <kentoo.zhao@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2008-10-30 15:26:05 +0000

    add arguments count checking

--- trunk/cpp/samples/Main.cxx
+++ trunk/cpp/samples/Main.cxx
@@ -7,9 +7,14 @@ using namespace cocoxml;
 
 int main(int argc, char **argv)
 {
+    if (argc != 2){
+        std::cerr << "Usage:" << argv[0] << " <xml file>\n" << std::endl;
+        return 1;
+    }
     XmlScanner *scanner = new XmlScanner(coco_string_create(argv[1]));
     XmlParser *parser = new XmlParser(scanner);
     
     parser->Parse();
     std::cout << parser->rss << std::endl;
+    return 0;
 }
