1beb274c9cf30058e9f5cbe5178b106df2b1147d
charlesw123456 <charlesw123456@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2008-12-05 06:04:54 +0000

    * Add CcSymbolTable_CheckTerminal and use it to simplify the others.

--- trunk/generic/algorithm/SymbolTable.c
+++ trunk/generic/algorithm/SymbolTable.c
@@ -41,14 +41,22 @@ CcSymbolTable_Destruct(CcSymbolTable_t * self)
     CcArrayList_Destruct(&self->pragmas);
 }
 
+const CcSymbol_t *
+CcSymbolTable_CheckTerminal(const CcSymbolTable_t * self, const char * name)
+{
+    CcArrayListIter_t iter; const CcSymbol_t * sym;
+    for (sym = (const CcSymbol_t *)CcArrayList_FirstC(&self->terminals, &iter);
+	 sym; sym = (const CcSymbol_t *)CcArrayList_NextC(&self->terminals, &iter))
+	if (!strcmp(sym->name, name)) return sym;
+    return NULL;
+}
+
 CcsBool_t
 CcSymbolTable_NewTerminalWithCheck(CcSymbolTable_t * self, const char * name,
 				   int line)
 {
-    CcArrayListIter_t iter; CcSymbol_t * sym;
-    for (sym = (CcSymbol_t *)CcArrayList_First(&self->terminals, &iter);
-	 sym; sym = (CcSymbol_t *)CcArrayList_Next(&self->terminals, &iter))
-	if (!strcmp(sym->name, name)) return FALSE;
+    const CcSymbol_t * sym = CcSymbolTable_CheckTerminal(self, name);
+    if (sym) return FALSE;
     CcArrayList_New(&self->terminals, (CcObject_t *)CcSymbolT(name, line));
     return TRUE;
 }
