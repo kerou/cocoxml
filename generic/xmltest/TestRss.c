30946a6822fbfff88f0a6e57c33906c9d2044fe8
charlesw123456 <charlesw123456@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2008-12-09 12:50:50 +0000

    * Add more test.

--- generic/xmltest/TestRss.c
+++ generic/xmltest/TestRss.c
@@ -23,6 +23,40 @@
 -------------------------------------------------------------------------*/
 #include  "Parser4Xml.h"
 
+static void
+ShowItem(const CcItem_t * self)
+{
+    printf("    Item:\n");
+    printf("        Title: [%s]\n", self->title);
+    printf("        Link: [%s]\n", self->link);
+}
+
+static void
+ShowChannel(const CcChannel_t * self)
+{
+    const CcItem_t * item; CcArrayListIter_t iter;
+    printf("Channel:\n");
+    printf("    Title: [%s]\n", self->title);
+    printf("    Link: [%s]\n", self->link);
+    for (item = (const CcItem_t *)CcArrayList_FirstC(&self->itemList, &iter);
+	 item;
+	 item = (const CcItem_t *)CcArrayList_NextC(&self->itemList, &iter))
+	ShowItem(item);
+    printf("    %d Item(s)\n", self->itemList.Count);
+}
+
+static void
+ShowRss(const CcRss_t * self)
+{
+    const CcChannel_t * channel; CcArrayListIter_t iter;
+    for (channel = (const CcChannel_t *)
+	     CcArrayList_FirstC(&self->channelList, &iter);
+	 channel; channel = (const CcChannel_t *)
+	     CcArrayList_NextC(&self->channelList, &iter))
+	ShowChannel(channel);
+    printf("%d Channel(s)\n", self->channelList.Count);
+}
+
 int
 main(int argc, char * argv[])
 {
@@ -34,6 +68,7 @@ main(int argc, char * argv[])
     if (!RssParser(&parser, argv[1], stderr)) goto errquit0;
     RssParser_Parse(&parser);
     if (!RssParser_Finish(&parser)) goto errquit1;
+    ShowRss(&parser.rss);
     RssParser_Destruct(&parser);
     return 0;
  errquit1:
