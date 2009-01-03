d0f26c0036076989f19bb66a02021875b597cb56
charlesw123456 <charlesw123456@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2009-01-03 07:40:19 +0000

    * Implement more.

--- applications/pgn/main.c
+++ applications/pgn/main.c
@@ -17,10 +17,35 @@
 -------------------------------------------------------------------------*/
 #include  "Parser.h"
 
+#define LNWIDTH  60
+#define LNWIDTH2 30
+
+static void
+PgnGame_Show(PgnGame_t * game)
+{
+    char lnbuf[LNWIDTH + 1], sidebuf[LNWIDTH2 + 1];
+    lnbuf[LNWIDTH] = 0;
+
+    memset(lnbuf, '-', LNWIDTH);
+    printf("%s\n", lnbuf);
+
+    memset(lnbuf, ' ', LNWIDTH);
+    lnbuf[LNWIDTH2 - 1] = 'v'; lnbuf[LNWIDTH2] = 's';
+    snprintf(sidebuf, LNWIDTH2 - 3, "%s(%d)", game->White, game->WhiteElo);
+    memcpy(lnbuf + 1 + (LNWIDTH2 - 3 - strlen(sidebuf)) / 2,
+	   sidebuf, strlen(sidebuf));
+    snprintf(sidebuf, sizeof(sidebuf), "%s(%d)", game->Black, game->BlackElo);
+    memcpy(lnbuf + LNWIDTH2 + 2 + (LNWIDTH2 - 3 - strlen(sidebuf)) / 2,
+	   sidebuf, strlen(sidebuf));
+    printf("%s\n", lnbuf);
+    printf("------------------------------------------------------------\n");
+}
+
 int
 main(int argc, char * argv[])
 {
     PgnParser_t parser;
+    PgnGame_t * cur;
 
     if (argc != 2) {
 	fprintf(stderr, "%s PGN-FILENAME\n", argv[0]);
@@ -28,6 +53,8 @@ main(int argc, char * argv[])
     }
     if (!PgnParser(&parser, argv[1], stderr)) goto errquit0;
     PgnParser_Parse(&parser);
+    for (cur = parser.firstGame; cur; cur = cur->next)
+	PgnGame_Show(cur);
     PgnParser_Destruct(&parser);
     return 0;
  errquit0:
