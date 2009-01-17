e869b01f88bd328631767a0167b4d6ba61a0265d
charlesw123456 <charlesw123456@0adc3070-8ce4-11dd-aa62-cdabd1f4ca1e>
2009-01-17 04:58:35 +0000

    * Setup incdirs.

--- trunk/applications/Kconfig/main.c
+++ trunk/applications/Kconfig/main.c
@@ -17,30 +17,34 @@
 -------------------------------------------------------------------------*/
 #include  "desc/Parser.h"
 #include  "file/Parser.h"
+#include  "c/IncPathList.h"
+#include  <limits.h>
 
 int
 main(int argc, char * argv[])
 {
     KcParser_t kcparser;
     CfParser_t cfparser;
+    char configdir[PATH_MAX];
 
     if (argc != 3) {
 	fprintf(stderr, "%s Kconfig-FILENAME Config-FILENAME\n", argv[0]);
 	goto errquit0;
     }
 
+    CcsDirname(configdir, sizeof(configdir), argv[2]);
     if (!strcmp(argv[1], "-")) {
 	if (!KcParser(&kcparser, stdout, stderr)) goto errquit0;
     } else {
 	if (!KcParser_ByName(&kcparser, argv[1], stderr)) goto errquit0;
     }
-    KcParser_Parse(&kcparser);
-
+    kcparser.incdirs = CcsIncPathListV(FALSE, FALSE, configdir, NULL);
     if (!strcmp(argv[2], "-")) {
 	if (!CfParser(&cfparser, stdout, stderr)) goto errquit1;
     } else {
 	if (!CfParser_ByName(&cfparser, argv[2], stderr)) goto errquit1;
     }
+    KcParser_Parse(&kcparser);
     CfParser_Parse(&cfparser);
 
     CfParser_Destruct(&cfparser);
